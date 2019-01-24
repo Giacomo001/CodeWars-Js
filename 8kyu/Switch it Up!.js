4369cb89ce9a557ae4bab73ea0f4b2147fd13197
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-01-24 22:25:02 +0100

    Create Switch it Up!.js

---
+++ 8kyu/Switch it Up!.js
@@ -0,0 +1,47 @@
+/* When provided with a number between 0-9, return it in words.
+Input :: 1
+Output :: "One".
+Try using "Switch" statements.
+*/
+
+//Solution with the 'switch' statement
+function switchItUp(number){
+    switch(number) {
+      case 0:
+        return 'Zero';      
+      case 1:
+        return 'One';      
+      case 2:
+        return 'Two';;      
+      case 3:
+        return 'Three';      
+      case 4:
+        return 'Four';      
+      case 5:
+        return 'Five';      
+      case 6:
+        return 'Six';      
+      case 7:
+        return 'Seven';      
+      case 8:
+        return 'Eight';      
+      case 9:
+        return 'Nine';       
+      default:
+        return 'NaN';
+    }
+}
+
+//Solution with arrays
+const switchItUp = n => [
+    'Zero',
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine',
+][n];
