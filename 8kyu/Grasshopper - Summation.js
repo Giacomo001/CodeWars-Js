59ad7609cdbd4f91654a94233c510b3b69de2623
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-01-24 22:13:36 +0100

    Create Grasshopper - Summation.js

---
+++ 8kyu/Grasshopper - Summation.js
@@ -0,0 +1,23 @@
+/* Write a program that finds the summation of every number from 1 to num. The number will always 
+be a positive integer greater than 0.
+
+For example:
+summation(2) -> 3
+1 + 2
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+*/
+
+//Solution with normal for loop
+var summation = function (num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+      sum += i;
+  }
+  return sum;
+}
+
+// Solution with math formula
+function summation(num) {
+  return (num * (num + 1) / 2);
+}
