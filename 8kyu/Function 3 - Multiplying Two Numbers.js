d7b1b68e01534bfcd9e96e5f2f468343b58996ee
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-01-28 16:24:30 +0100

    Create Function 3 - Multiplying Two Numbers.js

---
+++ 8kyu/Function 3 - Multiplying Two Numbers.js
@@ -0,0 +1,11 @@
+/* This function has to be called multiply and needs to take two numbers as arguments, 
+and has to return the multiplication of the two arguments.
+*/
+
+//Short solution
+const multiply = (a, b) => (a * b);
+
+//Long solution
+function multiply(a, b) {
+  return (a * b)
+}
