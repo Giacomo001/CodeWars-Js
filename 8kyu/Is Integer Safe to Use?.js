c98ed0874f732cd2e84fc50cabdd6859d32a2187
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-01-31 16:11:35 +0100

    Create Is Integer Safe to Use?.js

---
+++ 8kyu/Is Integer Safe to Use?.js
@@ -0,0 +1,15 @@
+/* Not all integers can be represented by JavaScript/TypeScript. It has space to to represent 53bit signed integers. 
+In this Kata, we've to determine if it is safe to use the integer or not. Make use of the latest ES6 features to find this.
+
+SafeInteger(9007199254740990) //true
+SafeInteger(-90) //true
+SafeInteger(9007199254740992) //false
+*/
+
+//Short solution
+const SafeInteger = n => (Number.isSafeInteger(n));
+
+//Long solution
+function SafeInteger(n) {
+  return Number.isSafeInteger(n);
+}
