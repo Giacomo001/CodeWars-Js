3b209bbdbe3d8ce602a735d28687c55fa449cc03
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-01-31 16:46:45 +0100

    Create Get Ascii Value of Character.js

---
+++ 8kyu/Get Ascii Value of Character.js
@@ -0,0 +1,9 @@
+/* Get ascii value of character
+
+Write a function getASCII which inputs a character and returns the corresponding ascii value for that character. 
+
+Example : getASCII('A') => 65
+*/
+
+//Short solution
+const getASCII = c => c.charCodeAt(0);
