38533515af84f8f53352cb0c44ae86f305fdaa7e
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-02 11:57:39 +0100

    Update Get Planet Name By ID.js

--- 8kyu/Get Planet Name By ID.js
+++ 8kyu/Get Planet Name By ID.js
@@ -4,32 +4,34 @@ getPlanetName(3); // should return 'Earth'
 
 //Just add the 'break'
 function getPlanetName(id){
-  var name;
+  var name
   switch(id){
     case 1:
-      name = 'Mercury';
-      break;
+      name = 'Mercury'
+      break
     case 2:
-      name = 'Venus';
-      break;
+      name = 'Venus'
+      break
     case 3:
-      name = 'Earth';
-      break;
+      name = 'Earth'
+      break
     case 4:
-      name = 'Mars';
-      break;
+      name = 'Mars'
+      break
     case 5:
-      name = 'Jupiter';
-      break;
+      name = 'Jupiter'
+      break
     case 6:
-      name = 'Saturn';
-      break;
+      name = 'Saturn'
+      break
     case 7:
-      name = 'Uranus';
-      break;
+      name = 'Uranus'
+      break
     case 8:
-      name = 'Neptune';
-      break;
+      name = 'Neptune'
+      break
+    default:
+      name = 'Not a planet'
    }
- return name;
+ return name
 }
