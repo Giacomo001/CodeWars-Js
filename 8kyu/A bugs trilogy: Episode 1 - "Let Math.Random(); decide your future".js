e5390f57b3ba96eeac30ed567dec5906cd7c8d89
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-02 17:15:23 +0100

    Create A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future".js

---
+++ 8kyu/A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future".js
@@ -0,0 +1,33 @@
+/* Our friend Pac has finally decided to pursue a career in the coding industry.
+He is a newbie, he needs to learn properly.
+Therefore, Pac wants to apply for the worldwide famous -and very demanding-
+'C0d3r 1ns1d3' Academy for beginners.
+In order to join, Pac is required to solve a series of 3 exercises about 'Bug Fixes'.
+He has been sent an email from the Academy with the following instructions:
+
+Dear Pac,
+This is the first exercise. Find out what's wrong and fix the code.
+You have 15 minutes to send a solution.
+Good Luck.
+This code is a mess! Would you help Pac to fix the code in time?
+
+(This might be helpful -> Math.Random())
+*/
+
+//Solution using string template and ternary operator
+const yourFutureCareer = () => {
+  let career = Math.random()
+  return `${(career <= 0.32) ? 'FrontEnd' : (career <= 0.65) ? 'BackEnd' : 'Full-Stack'} Developer`
+  }
+  
+//Long solution
+function yourFutureCareer(){
+  var career = Math.random();
+  if (career <= 0.32) {
+    return "Full-Stack Developer";
+  } else if (career <= 0.65) {
+    return "BackEnd Developer";
+  } else {
+    return "FrontEnd Developer";
+  }
+}
