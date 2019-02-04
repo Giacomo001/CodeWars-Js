972b3212aef102bdf51ab1a321f853ba77ad3452
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-04 18:16:03 +0100

    Create  String Ends With?.js

---
+++ 7kyu/String Ends With?.js
@@ -0,0 +1,17 @@
+/* Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+//Solution using '.endsWith.
+function solution(str, ending){
+  return str.endsWith(ending)
+}
+
+//Solution using regex
+function solution(str, ending){
+  return new RegExp(ending+"$", "i").test(str)
+}
