408bf897e8771139a446e0e6cde31f3659464629
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-04 17:28:44 +0100

    Create They say that Only the Name is Long Enough to Attract Attention. They also said that Only a Simple Kata will have Someone to Solve it. This is a Sadly Story #1: Are they Opposite?.js

---
+++ 8kyu/They say that Only the Name is Long Enough to Attract Attention. They also said that Only a Simple Kata will have Someone to Solve it. This is a Sadly Story #1: Are they Opposite?.js
@@ -0,0 +1,28 @@
+/* Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: 
+The result should be a boolean value, instead of a string.
+
+The opposite means: All letters of the two strings are the same, but the case is opposite. 
+You can assume that the string only contains letters or it's a empty string. 
+Also take note of the edge case - if both strings are empty then you should return false/False.
+
+#Some examples:
+isOpposite("ab","AB") should return true;
+isOpposite("aB","Ab") should return true;
+isOpposite("aBcd","AbCD") should return true;
+isOpposite("AB","Ab") should return false;
+isOpposite("","") should return false;
+*/
+
+////Solution with for loop
+function isOpposite(s1,s2){
+  if(s1 === s2 || s1.toLowerCase() !== s2.toLowerCase()) return false  
+  for(let i = 0; i < s1.length; i++) {
+    if(s1.charAt(i) === s2.charAt(i)) return false 
+  }
+  return true
+}
+
+//Smart solution using '.replace' method
+function isOpposite(s1,s2){
+  return s1 != '' && s1 == s2.replace(/./g, ch => ch['to' + ((ch <= 'Z') ? 'Lower' : 'Upper') + 'Case']())
+}
