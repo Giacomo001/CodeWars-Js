7af2f6413b4a6eff4e972e92932cc47d08cfe530
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-08 16:31:15 +0100

    Create Flatten.js

---
+++ 7kyu/Flatten.js
@@ -0,0 +1,15 @@
+/* Write a function that flattens an Array of Array objects into a flat Array. 
+Your function must only do one level of flattening.
+
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]
+*/
+
+//Short solution
+const flatten = array => array.reduce((arr, x) => arr.concat(x), [])
+
+//Another solution with diffent methods
+var flatten = function (lol){
+  return [].concat.apply([],lol);
+}
