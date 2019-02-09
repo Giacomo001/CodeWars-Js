678e532eb818e726deb99b1216b19ad11f693144
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-09 17:22:13 +0100

    Create Factorial Factory.js

---
+++ 7kyu/Factorial Factory.js
@@ -0,0 +1,12 @@
+/* In mathematics, the factorial of integer 'n' is written as 'n!'. It is equal to the product of n 
+and every integer preceding it. For example: 5! = 1 x 2 x 3 x 4 x 5 = 120
+
+Your mission is simple: write a function that takes an integer 'n' and returns 'n!'.
+
+You are guaranteed an integer argument. For any values outside the positive range, return null, nil or None.
+
+Note: 0! is always equal to 1. Negative values should return null;
+*/
+
+//Short solution
+const factorial = n => (n < 0) ? null : (n === 0) ? 1 : n * factorial(n - 1)
