14cb580005331c5524794065af5c16b5e90375c4
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-10 17:03:34 +0100

    Create Basic JS - Building a Calculator.js

---
+++ 7kyu/Basic JS - Building a Calculator.js
@@ -0,0 +1,41 @@
+/* Let's build a calculator.
+
+This is very basic Javascript kata.
+
+The test expects you to provide a Calculator object with the following methods:
+
+Calculator.add()
+Calculator.subtract()
+Calculator.multiply()
+Calculator.divide()
+Each method expects two arguments, so for instance:
+
+Calculator.add(1,4) should return 5.
+
+Only integers are expected to be passed in as arguments, and the divide method should return 'false' 
+when trying the divide by zero.
+*/
+
+//Solution following the exercise
+var Calculator = {
+  add: (x, y) => x + y,
+  subtract: (x, y) => x - y,
+  multiply: (x, y) => x * y,
+  divide: (x, y) => (y === 0) ? false : x / y
+}
+
+//Long solution
+var Calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return b === 0 ? false : a / b;
+  }
+}
