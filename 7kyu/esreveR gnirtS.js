4e3814cb70f7e9c16bd0166dfe7529c4dad85e94
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-11 17:40:38 +0100

    Create esreveR gnirtS.js

---
+++ 7kyu/esreveR gnirtS.js
@@ -0,0 +1,14 @@
+/* Create a function called reverse for the String prototype that will allow the following functionality:
+
+"String".reverse();// => returns "gnirtS"
+"Super awesome string".reverse();// => returns "gnirts emosewa repuS"
+
+The reverse function should NOT modify the original string.
+*/
+
+//Solution following the exercise
+String.prototype.reverse = function() {
+  return this.split('')
+             .reverse()
+             .join('')
+}
