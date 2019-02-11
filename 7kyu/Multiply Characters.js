61302ed20b8b374f676b52f143d352da8adbb4e4
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-11 18:04:46 +0100

    Create Multiply Characters.js

---
+++ 7kyu/Multiply Characters.js
@@ -0,0 +1,16 @@
+/* Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+Expected results:
+
+spam(1);  // hue
+spam(6);  // huehuehuehuehuehue
+spam(14); // huehuehuehuehuehuehuehuehuehuehuehuehuehue
+*/
+
+//Short solution
+const spam = number => 'hue'.repeat(number)
+
+//Solution using an array
+function spam(number){
+  return Array(++number).join("hue");
+}
