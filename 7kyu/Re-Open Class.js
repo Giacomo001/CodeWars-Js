0be624b20cb8fb5cc22872419e67c1a942238272
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-16 16:16:12 +0100

    Create Re-Open Class.js

---
+++ 7kyu/Re-Open Class.js
@@ -0,0 +1,22 @@
+/* Ruby and Javascript let you re-open classes so you can add new functionalities to existing classes and objects.
+
+To demonstrate this, you'll have to add a new method (called my_new_method in Ruby or myNewMethod in JS) 
+into the String class that simply calls the upcase method (toUpperCase() in Javascript), so that:
+
+Ruby
+"abc".my_new_method
+
+JS
+"abc".myNewMethod();
+
+returns
+"ABC"
+*/
+
+//Long solution
+String.prototype.myNewMethod = function() {
+  return this.toUpperCase()
+}
+
+//Short solution
+String.prototype.myNewMethod = String.prototype.toUpperCase
