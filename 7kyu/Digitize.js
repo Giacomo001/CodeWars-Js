7954445ddf5e98ec6e8704ae4d585e5633133aa2
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-18 19:59:47 +0100

    Create Digitize.js

---
+++ 7kyu/Digitize.js
@@ -0,0 +1,14 @@
+/* Given a non-negative integer, return an array / a list of the individual digits in order.
+
+Examples:
+123 => [1,2,3]
+1 => [1]
+8675309 => [8,6,7,5,3,0,9]
+*/
+
+//Solution using methods
+const digitize = n => String(n).split('')
+                               .map(Number)    //Number is already a function
+                               
+//Short solution
+const digitize = n => [...n + ''].map(Number)
