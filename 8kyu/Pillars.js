85bfd8ec784d4090012fc3009fc5d6f2aa2d3324
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-02-19 12:58:56 +0100

    Create Pillars.js

---
+++ 8kyu/Pillars.js
@@ -0,0 +1,17 @@
+/* There are pillars near the road. The distance between the pillars is the same and the width of the 
+pillars is the same. 
+
+Your function accepts three arguments:
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first 
+and last pillar).
+*/
+
+//Long solution
+function pillars(num_pill, dist, width) {
+  dist = dist * 100
+  return (num_pill <= 1) ? 0 : (dist * (num_pill - 1)) + (width * (num_pill - 2))
+}
