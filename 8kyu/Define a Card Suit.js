20e73bbf8a958745eeafb147740c9bb730f4bed6
Giacomo Leoni <giacomo.leoni1992@gmail.com>
2019-02-25 11:41:46 +0100

    Create Define a Card Suit.js

---
+++ 8kyu/Define a Card Suit.js
@@ -0,0 +1,33 @@
+/* You get any card as an argument. Your task is to return a suit of this card.
+
+Our deck (is preloaded):
+
+deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+        '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+        '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+        '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+//Solution following the exercise
+function defineSuit(card) {
+  if(card.includes('♣')) return 'clubs'
+  if(card.includes('♠')) return 'spades'
+  if(card.includes('♦')) return 'diamonds'
+  if(card.includes('♥')) return 'hearts'
+}
+
+//Solution using an object(dictionary in python)
+function defineSuit(card) {
+  const s = {
+    "♣": "clubs",
+    "♠": "spades",
+    "♦": "diamonds",
+    "♥": "hearts"
+  }
+  return s[card.charAt(card.length - 1)]
+}
